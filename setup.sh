970f16cb14a4c70b27c43e78deee6e6c724b2063
Antoine Hérault <antoine.herault@gmail.com>
2011-11-08 19:32:56 +0100

    Improve setup.sh

--- setup.sh
+++ setup.sh
@@ -6,18 +6,25 @@ BASEDIR=$(dirname $0)
 
 VIMDIR=$BASEDIR/vim
 
+rm ~/.vim
 ln -s $VIMDIR ~/.vim
+
+rm ~/.vimrc
 ln -s $VIMDIR/vimrc ~/.vimrc
 
 # SETUP BASH
 
 BASHDIR=$BASEDIR/bash
 
+rm ~/.bashrc
 ln -s $BASHDIR/bashrc ~/.bashrc
 
 # SETUP GIT
 
 GITDIR=$BASEDIR/git
 
+rm ~/.gitconfig
 ln -s $GITDIR/gitconfig ~/.gitconfig
+
+rm ~/.gitignore
 ln -s $GITDIR/gitignore ~/.gitignore
