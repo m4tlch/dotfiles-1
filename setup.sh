c554291a4caa377b459bd2a5994e88965aad06d6
Antoine Hérault <antoine.herault@gmail.com>
2013-05-25 13:19:47 +0200

    Pimp my linux

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-BASEDIR=$(dirname $0)
+BASEDIR=`pwd`
 
 # SETUP VIM
 
@@ -18,6 +18,7 @@ BASHDIR=$BASEDIR/bash
 
 rm ~/.bashrc
 ln -s $BASHDIR/bashrc ~/.bashrc
+
 rm ~/.bash_profile
 ln -s $BASHDIR/bash_profile ~/.bash_profile
 
@@ -38,28 +39,4 @@ ln -s $GITDIR/gitconfig ~/.gitconfig
 rm ~/.gitignore
 ln -s $GITDIR/gitignore ~/.gitignore
 
-# SETUP SSH
 
-SSHDIR=$BASEDIR/ssh
-
-rm ~/.ssh
-ln -s $SSHDIR ~/.ssh
-
-# SETUP XDEFAULTS
-rm ~/.Xdefaults
-ln -s $BASEDIR/Xdefaults
-
-# SETUP RVM
-
-RVMDIR=$BASEDIR/rvm
-
-rm ~/.rvmrc
-ln -s $RVMDIR/rc ~/.rvmrc
-
-# SETUP LOCAL BINARIES
-
-sudo wget http://getcomposer.org/composer.phar -O /usr/local/bin/composer
-sudo chmod a+x /usr/local/bin/composer
-
-sudo wget http://cs.sensiolabs.org/get/php-cs-fixer.phar -O /usr/local/bin/php-cs-fixer
-sudo chmod a+x /usr/local/bin/php-cs-fixer
